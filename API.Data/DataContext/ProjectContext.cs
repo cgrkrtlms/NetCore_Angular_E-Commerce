b1209504c4af17efe2480cd0e1295f796124ddd3
İbrahim Çağrı Kurtulmuş <cagrikurtulmus@gmail.com>
2022-01-03 13:19:15 +0300

    Add project files.

---
+++ API.Data/DataContext/ProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using API.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Data.DataContext
+{
+   public class ProjectContext:DbContext
+    {
+        public ProjectContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
